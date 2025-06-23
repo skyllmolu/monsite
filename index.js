75d0f9a6390a15e911992fe768de96cdba1659be
skyllmolu <skyllmolu.s@gmail.com>
2025-06-23 16:11:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-   function toggleMenu() {
+ function toggleMenu() {
     const menu = document.getElementById("menuLinks");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   }
