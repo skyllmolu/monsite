5f00162bc0148a1a28d941128ea54ea7743a5ba7
skyllmolu <skyllmolu.s@gmail.com>
2025-06-25 13:53:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   }
 
+ 
+   window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+
   
